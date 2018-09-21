20cae4079080cff12b3bd28989b15b099386f4ce
Volizik <kizilov.yura@gmail.com>
2018-09-21 22:40:05 +0300

    it's alive!

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,21 @@
+// Vuex structure from https://habr.com/company/ruvds/blog/420357/
+
 import Vue from 'vue'
 import Vuex from 'vuex'
+import modules from './modules'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
+const store = new Vuex.Store({
+    modules,
+    strict: process.env.NODE_ENV !== 'production'
+});
 
-  },
-  mutations: {
+// Автоматически запустить действие `init` для каждого существующего модуля
+for (const moduleName of Object.keys(modules)) {
+    if (modules[moduleName].actions.init) {
+        store.dispatch(`${moduleName}/init`)
+    }
+}
 
-  },
-  actions: {
-
-  }
-})
+export default store
