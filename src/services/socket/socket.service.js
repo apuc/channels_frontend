1ab2d3b2e5730665c59994c8784689eac03e38b3
Volizik <kizilov.yura@gmail.com>
2018-11-23 16:27:19 +0300

    hotfix: fix socket url

--- src/services/socket/socket.service.js
+++ src/services/socket/socket.service.js
@@ -7,7 +7,7 @@ export let io = null;
 export function connectSocket(token) {
     return new Promise((resolve, reject) => {
 
-        io = socketIo(process.env.VUE_APP_SOCKET_URL, {query: {token: token}}); // пробуем подключится к ноду
+        io = socketIo('/', {query: {token: token}}); // пробуем подключится к ноду
 
         io.on('connect', () => {
             socketEventListenerInit();
