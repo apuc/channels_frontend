66ab8b01a386b85ee728aca0daa42c2bcb9ef4fe
yura <yurafront@gmail.com>
2018-12-03 11:16:19 +0300

    add avatar to messages

--- src/components/chat/ChatMessage.vue
+++ src/components/chat/ChatMessage.vue
@@ -3,7 +3,7 @@
     <b-media>
       <div class="flex">
         <a :href="'/user/'" @click="openUserProfile($event, messageData.user)">
-          <img :src="noavatar" width="64" alt="placeholder"/>
+          <img :src="messageData.from.avatar || noavatar" width="64" alt="placeholder"/>
         </a>
 
         <h5 class="mt-0 message-title" >
@@ -30,7 +30,6 @@
     },
     data() {
       return {
-        myId: 111,
         noavatar: 'data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAwAAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFzByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSpa/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJlZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uisF81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PHhhx4dbgYKAAA7'
       }
     },
