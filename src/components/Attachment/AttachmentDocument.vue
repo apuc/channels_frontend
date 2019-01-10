1b2d7c819a855187f76479e90989eabdfb2fa4ac
Volizik <kizilov.yura@gmail.com>
2019-01-10 23:23:47 +0300

    стилизовал два типа атачментов

---
+++ src/components/Attachment/AttachmentDocument.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="doc-attachment">
+        <img :src="data.img_url" alt="document icon">
+        <div class="doc-attachment-data">
+            <span class="name">{{ data.name }}</span>
+            <span class="weight">{{ data.weight }}</span>
+        </div>
+        <div class="close" @click="REMOVE_ATTACHMENT(data.id)">
+            <v-icon scale="1" class="icon" name="times-circle"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapMutations} from 'vuex';
+
+    export default {
+        name: 'AttachmentDocument',
+        props: {
+            data: {
+                type: Object,
+                required: true
+            }
+        },
+        methods: {
+            ...mapMutations({
+                REMOVE_ATTACHMENT: 'messages/REMOVE_ATTACHMENT',
+            }),
+        },
+    }
+</script>
+
+<style>
+    .doc-attachment {
+        margin: 10px 0;
+        display: flex;
+        align-items: center;
+        width: 100%;
+    }
+    .doc-attachment img {
+        margin-right: 10px;
+        display: block;
+        width: 50px;
+        height: 50px;
+        flex-shrink: 0;
+        object-fit: contain;
+    }
+    .doc-attachment-data {
+        display: flex;
+        flex-direction: column;
+    }
+    .close {
+        display: block;
+        margin-left: auto;
+    }
+</style>
--- src/components/Attachment/AttachmentImage.vue
+++ src/components/Attachment/AttachmentImage.vue
@@ -1,9 +1,56 @@
 <template>
-  <h1>Image</h1>
+    <div class="attachment-image">
+        <img :src="data.url" alt="attachment image">
+        <div class="close-btn" @click="REMOVE_ATTACHMENT(data.id)">
+            <v-icon scale="1" class="icon" name="times-circle"/>
+        </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'ImageAttachment'
-  }
+    import {mapMutations} from 'vuex';
+
+    export default {
+        name: 'AttachmentImage',
+        props: {
+            data: {
+                required: true,
+                type: Object
+            }
+        },
+        methods: {
+            ...mapMutations({
+                REMOVE_ATTACHMENT: 'messages/REMOVE_ATTACHMENT',
+            }),
+        },
+    }
 </script>
+
+<style>
+    .attachment-image {
+        position: relative;
+        padding: 10px 0;
+        width: 100px;
+        height: 100px;
+    }
+
+    .attachment-image img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    
+    .close-btn {
+        position: absolute;
+        right: -5px;
+        top: 0;
+        z-index: 1;
+        display: none;
+        cursor: pointer;
+    }
+    
+    .attachment-image:hover .close-btn {
+        display: block;
+    }
+</style>
