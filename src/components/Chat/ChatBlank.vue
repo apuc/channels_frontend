e63135a41ac39c43825c702e2292c49879bf0aa8
skat <kirill712@ukr.net>
2019-10-21 12:31:38 +0300

    popular channels

--- src/components/Chat/ChatBlank.vue
+++ src/components/Chat/ChatBlank.vue
@@ -1,10 +1,46 @@
 <template>
-  <p class="info-message text-uppercase mt-4">Чтобы началь диалог выберите пользователя или канал</p>
+  <div class="popular-channels">
+    <NavSectionChannels v-for="channel in channels"
+                        :type="'channel'"
+                        :data="channel"
+                        :notification="false"
+    />
+  </div>
 </template>
 
 <script>
+    import Vue from "vue";
+    import NavSectionChannels from '../Nav/NavSectionChannels';
+  
   export default {
     name: "ChatBlank",
+      
+    components:{
+        NavSectionChannels, 
+    },  
+      
+    data(){
+      return {
+          channels:[],
+      }  
+    },
+      
+    created(){
+        this.getChannels();
+    },  
+    
+    methods:{
+       getChannels(){
+           Vue.http.get(`${process.env.VUE_APP_API_URL}/channels/popular`)
+               .then(
+                   res => {
+                       this.channels = res.body.data;
+                       console.log(this.channels);
+                   },
+                   err => console.log('err get channels', err)
+               )
+       } 
+    },  
   }
 </script>
 
@@ -14,4 +50,11 @@
     color: #ccc;
     text-align: center;
   }
+
+  .popular-channels{
+    padding: 20px;
+    max-height: 100%;
+    overflow-y: auto;
+  }
+  
 </style>
