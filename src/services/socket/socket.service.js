eaf8b3a47bc4234febdec3009a5453169fc13ea3
skat <kirill712@ukr.net>
2020-03-30 15:18:40 +0300

    debug socket

--- src/services/socket/socket.service.js
+++ src/services/socket/socket.service.js
@@ -12,9 +12,57 @@ export function connectSocket(token, user_id) {
         io = socketIo(process.env.VUE_APP_SOCKET_URL, {query: {token, user_id}}); // пробуем подключится к ноду
 
         io.on('connect', () => {
+            console.log('event_connect')
             socketEventListenerInit();
             resolve(io)
         });
+
+        io.on('connect_error', (error) => {
+          console.log('connect_error',error)
+          reject(error)
+        });
+
+        io.on('connect_timeout', (timeout) => {
+          console.log('connect_timeout',timeout)
+          reject(timeout)
+        });
+
+        io.on('disconnect', (reason) => {
+          
+          console.log('disconnect',reason)
+          
+          if (reason === 'io server disconnect') {
+            console.log('disconnect_serv',reason)
+          }
+         
+          reject(reason)
+        });
+
+        io.on('reconnect', (attemptNumber) => {
+          console.log('reconnect',attemptNumber)
+          reject(attemptNumber)
+        });
+        
+        io.on('reconnect_attempt', (attemptNumber) => {
+          console.log('reconnect_attempt',attemptNumber)
+          reject(attemptNumber)
+        });
+        
+        io.on('reconnecting', (attemptNumber) => {
+          console.log('reconnecting',attemptNumber)
+          reject(attemptNumber)
+        });
+        
+        io.on('reconnect_error', (error) => {
+          console.log('reconnect_error',error)
+          reject(error)
+        });
+        
+        io.on('reconnect_failed', () => {
+          console.log('reconnect_failed')
+          reject()
+        });
+        
         io.on('error', (error) => {
             reject(error)
         });
